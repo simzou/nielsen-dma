c84f26a4f704e32d75c5e52e274fe82cba0366a0
Andrew Ashbacher <andrewashbacher@gmail.com>
2016-11-23 00:07:01 -0800

    upgraded to d3.js v4; added market breakdowns

--- aa.js
+++ aa.js
@@ -1,20 +1,37 @@
+Array.prototype.shuffle = function() {
+  var i = this.length, j, temp;
+  if ( i == 0 ) return this;
+  while ( --i ) {
+     j = Math.floor( Math.random() * ( i + 1 ) );
+     temp = this[i];
+     this[i] = this[j];
+     this[j] = temp;
+  }
+  return this;
+}
+
 var clusters = [];
 var cluster_counter = 0;
-var population_format = new Intl.NumberFormat("en-US");
+var population_format = new Intl.NumberFormat("en-US", { maximumFractionDigits: 0 });
+var active_cluster_tr = null;
+
+var total_samsung_televisions = 16431973;
 
 class MarketProcessor {
 
   constructor(dma_objects) {
     this.dma_objects = dma_objects;
     this.total_population = this.count_population(this.dma_objects.fills[0]);
+    this.samsung_percentage = total_samsung_televisions / this.total_population;
   }
-
 
   process() {
 
+    this.write_overview();
+
     this.clusters = [];
 
-    this.divide_into(8, this.dma_objects.fills[0]);
+    this.divide_into(6, this.dma_objects.fills[0]);
 
     this.color_parts(this.final_parts);
 
@@ -23,6 +40,14 @@ class MarketProcessor {
       var cluster_id = ++cluster_counter;
       var population = this.count_population(this.final_parts[i]);
       var cluster = { id: cluster_id, parts: this.final_parts[i], population: population };
+
+      for (var j = 0; j < this.final_parts[i].length; j++) {
+        var part = d3.select(this.final_parts[i][j]);
+        var data = part.data()[0].properties;
+        data.cluster_id = cluster_id;
+        part.data(data);
+      }
+
       this.clusters.push(cluster);
     }
 
@@ -39,7 +64,7 @@ class MarketProcessor {
   divide_to(target_population, fills, iteration) {
     if (iteration == null) { iteration = 1; }
 
-    var direction = ['longitude', 'latitude'][iteration % 2];
+    var direction = ['latitude', 'longitude'][iteration % 2];
 
     var output_parts = []
 
@@ -122,22 +147,37 @@ class MarketProcessor {
     var hsl_increment = Math.floor(360 / parts_count); // 0-360
     var s = 0.5; // 0-1
     var l = 0.5; // 0-1
+    var colors = [];
     for (var i = 0; i < parts_count; i++) {
       var h = hsl_increment * (i + 1);
       var color = d3.hsl(h, 0.5, 0.5);
+      colors.push(color);
+    }
+    colors.shuffle();
+    for (i = 0; i < parts_count; i++) {
       for (var j = 0; j < parts[i].length; j++) {
         var d = d3.select(parts[i][j]);
         //d.attr('fill', 'hsl(' + h + ', ' + s + ', ' + l + ')');
-        d.attr('fill', color);
+        d.attr('fill', colors[i]);
         //d3.select(parts[i][j]).dispatch('highlight');
       }
     }
   }
 
+  write_overview() {
+    var $overview = d3.select('#overview').append('table');
+    $overview.append('tr').html("<th>Total TV Households</th><th>Total Samsung TVs</th><th>% of Total</th>");
+    $overview = $overview.append('tbody');
+    var html = "<td>" + population_format.format(this.total_population) + "</td>";
+    html    += "<td>" + population_format.format(total_samsung_televisions) + "</td>";
+    html    += "<td>" + Number(this.samsung_percentage * 100).toFixed(1) + "%</td>";
+    $overview.append('tr').html(html);
+  }
+
   write_clusters() {
 
     var $cluster_list = d3.select('#cluster_list').append('table');
-    $cluster_list.append('tr').html("<th>ID</th><th>Population</th><th>Market Count</th>");
+    $cluster_list.append('tr').html("<th>ID</th><th>TV Households</th><th>Samsung TVs</th><th># Markets</th>");
     $cluster_list = $cluster_list.append('tbody');
 
     for (var i = 0; i < this.clusters.length; i++) {
@@ -148,6 +188,7 @@ class MarketProcessor {
   write_cluster($cluster_list, cluster) {
     var html = "<td>" + cluster.id + "</td>";
     html    += "<td class='number'>" + population_format.format(cluster.population) + "</td>";
+    html    += "<td class='number'>" + population_format.format(cluster.population * this.samsung_percentage) + "</td>";
     html    += "<td class='number'>" + cluster.parts.length + "</td>";
 
     //var cluster_html = d3.creator('p').html(string);
@@ -157,6 +198,7 @@ class MarketProcessor {
 
     $cluster_list
       .append('tr')
+      .attr('id', "cluster_" + cluster.id)
       .html(html)
       .on('mouseover', function (d) {
         for (var j = 0; j < cluster.parts.length; j++) {
@@ -164,6 +206,11 @@ class MarketProcessor {
           original_fill = dd.attr('fill');
           dd.attr("fill", "orange");
         }
+        that.set_active_cluster_tr(this);
+        that.display_cluster_detail(cluster);
+      })
+      .on('activate', function (d) {
+        that.set_active_cluster_tr(this);
         that.display_cluster_detail(cluster);
       })
       .on('mouseout', function (d) {
@@ -172,32 +219,71 @@ class MarketProcessor {
           //original_fill = dd.attr('fill');
           dd.attr("fill", original_fill);
         }
+      })
+      .on('deactivate', function (d) {
+        d3.select(this).classed('active', false);
       });
+  }
+
+  set_active_cluster_tr(tr) {
+    if (active_cluster_tr != null) {
+      d3.select(active_cluster_tr).classed('active', false);
+    }
+    d3.select(tr).classed('active', true);
+    active_cluster_tr = tr;
   }
 
   display_cluster_detail(cluster) {
     var $cluster_detail = d3.select('#cluster_detail').html(null).append('table');
 
     var table_headers = "<th>Market</th>";
-    table_headers    += "<th>Population</th>";
+    table_headers    += "<th>TV Households</th>";
+    table_headers    += "<th>Samsung TVs</th>";
     table_headers    += "<th>% with Cable</th>";
     table_headers    += "<th>Nielsen Rank</th>";
 
     $cluster_detail.append('tr').html(table_headers);
 
+    $cluster_detail = $cluster_detail.append('tbody');
+
     for (var i = 0; i < cluster.parts.length; i++) {
-      var cluster_part = cluster.parts[i];
-      var data = d3.select(cluster_part).data()[0].properties;
-      console.log(cluster_part);
-      console.log(data.dma1);
-      var html = "<td>" + data.dma1 + "</td>";
-      html    += "<td class='number'>" + population_format.format(parseInt(data['TV Homes'], 10)) + "</td>";
-      html    += "<td class='number'>" + data.cableperc + "</td>";
-      html    += "<td class='number'>" + data.Rank + "</td>";
-      $cluster_detail
-        .append('tr')
-        .html(html);
+      var $cluster_part = d3.select(cluster.parts[i]);
+      this.write_market($cluster_detail, $cluster_part);
     }
+  }
+
+  write_market($cluster_detail, $cluster_part) {
+    var data = $cluster_part.data()[0].properties;
+
+    var tv_homes = parseInt(data['TV Homes'], 10);
+
+    var html = "<td>" + data.dma1 + "</td>";
+    html    += "<td class='number'>" + population_format.format(tv_homes) + "</td>";
+    html    += "<td class='number'>" + population_format.format(tv_homes * this.samsung_percentage) + "</td>";
+    html    += "<td class='number'>" + Number(data.cableperc).toFixed(1) + "%</td>";
+    html    += "<td class='number'>" + data.Rank + "</td>";
+
+    var original_fill = null;
+
+    $cluster_detail
+      .append('tr')
+      .attr('id', "market_" + data.dma)
+      .html(html)
+      .on('mouseover', function (d) {
+        original_fill = $cluster_part.attr('fill');
+        $cluster_part.attr("fill", "orange");
+        // The following draws a small black circle at the lat/lon of the market.
+        //svg.append("circle").attr("r", 5).attr("transform", function() {return "translate(" + projection([data.longitude,data.latitude]) + ")";});
+      })
+      .on('activate', function (d) {
+        d3.select(this).classed('active', true);
+      })
+      .on('mouseout', function (d) {
+        $cluster_part.attr("fill", original_fill);
+      })
+      .on('deactivate', function (d) {
+        d3.select(this).classed('active', false);
+      });
   }
 
 }
--- main.js
+++ main.js
@@ -2,19 +2,18 @@
 var width = 800,
     height = 500;
 // sets the type of view
-var projection = d3.geo.albersUsa()
+var projection = d3.geoAlbersUsa()
     .scale(1070) // size, bigger is bigger
     .translate([width / 2, height / 2]);
 
 //creates a new geographic path generator
-var path = d3.geo.path().projection(projection);
-var xScale = d3.scale.linear()
+var path = d3.geoPath().projection(projection);
+var xScale = d3.scaleLinear()
     .domain([0, 7])
     .range([0, 500]);
 
-var xAxis = d3.svg.axis()
+var xAxis = d3.axisBottom()
     .scale(xScale)
-    .orient("bottom")
     .tickSize(13)
     .tickFormat(d3.format("0.0f"));
 
@@ -26,7 +25,7 @@ var svg = d3.select("svg")
       .attr("width", width)
       .attr("height", height)
 
-var graticule = d3.geo.graticule()
+var graticule = d3.geoGraticule()
     .extent([[-98 - 45, 38 - 45], [-98 + 45, 38 + 45]])
     .step([5, 5]);
 
@@ -85,20 +84,30 @@ d3.json("nielsentopo.json", function(error, dma) {
 
       var prop = d.properties;
 
-      var string = "<p><strong>Market Area Name</strong>: " + prop.dma1 + "</p>";
-      string += "<p><strong>Homes with TVs</strong>: " + numberWithCommas(prop["TV Homes"]) + "</p>";
-      string += "<p><strong>% with Cable</strong>: " + prop.cableperc + "%</p>";
-      string += "<p><strong>Nielsen Rank</strong>: " + prop.Rank + "</p>";
+      // var string = "<p><strong>Market Area Name</strong>: " + prop.dma1 + "</p>";
+      // string += "<p><strong>Homes with TVs</strong>: " + numberWithCommas(prop["TV Homes"]) + "</p>";
+      // string += "<p><strong>% with Cable</strong>: " + prop.cableperc + "%</p>";
+      // string += "<p><strong>Nielsen Rank</strong>: " + prop.Rank + "</p>";
 
-      d3.select("#textbox")
-        .html("")
-        .append("text")
-        .html(string)
+      // d3.select("#textbox")
+      //   .html("")
+      //   .append("text")
+      //   .html(string);
+    
+      d3.select('#cluster_' + prop.cluster_id)
+        .dispatch('activate');
+      d3.select('#market_' + prop.dma)
+        .dispatch('activate');
     })
 
-    .on("mouseout", function(d){
+    .on("mouseout", function(d) {
+      var prop = d.properties;
       d3.select(this)
-      .attr("fill", original_fill_color)
+        .attr("fill", original_fill_color);
+      d3.select('#cluster_' + prop.cluster_id)
+        .dispatch('deactivate');
+      d3.select('#market_' + prop.dma)
+        .dispatch('deactivate');
     })
 
     .on("highlight", function (d) {
@@ -119,18 +128,20 @@ d3.json("nielsentopo.json", function(error, dma) {
       .attr("id", "dma-borders")
       .attr("d", path);
 
-    dma_objects.fills    = dma_objects.fills;
-    dma_objects.outlines = dma_objects.outlines;
+    dma_objects.fills    = dma_objects.fills._groups;
+    dma_objects.outlines = dma_objects.outlines._groups;
 
     window.market_processor = new MarketProcessor(dma_objects);
     window.market_processor.process();
+
+
 
   }) // END d3.json('tv.json', ...)
 
 }) // END d3.json('nielsentopo.json', ...)
 
-// via http://stackoverflow.com/a/2901298
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+// // via http://stackoverflow.com/a/2901298
+// function numberWithCommas(x) {
+//     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// }
 
